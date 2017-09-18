c5848450326e290612c371b43196ba89fd537e89
Jose Melendez <JoseM_Melendez@icloud.com>
2017-09-18 18:11:20 -0400

    Added randomization

--- DiceShaker/DiceShaker/ViewController.swift
+++ DiceShaker/DiceShaker/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    
+    var randomDiceIndex2 : Int = 0
 
+    //IB-Outlets
     @IBOutlet weak var diceImageView1: UIImageView!
     
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -24,6 +29,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //IB-Actions
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        
+        //Generate random number between 0-5
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        
+        //Generate random number between 0-5
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+    }
+    
 }
 
